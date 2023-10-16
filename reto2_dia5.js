efca172b2e61cbd9f4ff81aa84590f6515080243
MarcoWiens <marco.wiens@gmail.com>
2023-10-16 12:21:46 +0200

    Primer commit con funciones.

---
+++ reto2_dia5.js
@@ -0,0 +1,22 @@
+
+function suma(op1,op2){
+    return op1 + op2;
+}
+
+function resta(op1,op2){
+    return op1 - op2;
+}
+
+function multiplicar(op1,op2){
+    return op1 * op2;
+}
+
+function division(op1,op2){
+    return op1 / op2;
+}
+
+function cuadrado(num){
+    return num * num;
+}
+
+module.exports = {suma, resta, multiplicar, division, cuadrado};
---
+++ run.js
@@ -0,0 +1,8 @@
+let myLib = require("./reto2_dia5");
+
+//RETO 2
+console.log(myLib.suma(2,4));
+console.log(myLib.multiplicar(5,3));
+console.log(myLib.division(8,2));
+console.log(myLib.resta(7,5));
+console.log(myLib.cuadrado(5));
